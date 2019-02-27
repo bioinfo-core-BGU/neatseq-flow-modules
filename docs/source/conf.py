9cf06d46eae112bfce9b7db1dc1aaf27ceba7455
sklarz <sklarz@bgu.ac.il>
2019-02-27 16:23:19 +0200

    * favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -166,7 +166,7 @@ html_baseurl = "https://neatseq-flow3.readthedocs.io/en/latest/#"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "figs/favicon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
