a7fea1a72f9e472d1e8ce16da9ffdd1fa59c6aac
sklarz <sklarz@bgu.ac.il>
2019-05-14 10:10:37 +0300

    * Fixed issue with temp dir in new samtools

--- neatseq_flow_modules/main_NSF_classes/mapping/samtools_new.py
+++ neatseq_flow_modules/main_NSF_classes/mapping/samtools_new.py
@@ -289,7 +289,6 @@ class Step_samtools_new(Step):
 ##########
 # Making local link to original bam file: (-f to force)
 #----------
-mkdir -p {use_dir}
 cp -fs \\
 \t{active_file} \\
 \t{use_dir}
@@ -297,7 +296,7 @@ cp -fs \\
 """.format(active_file=active_files[active_type],
            use_dir=use_dir)
 
-            active_files[active_type] = temp_use_dir + os.path.basename(active_files[active_type])
+            active_files[active_type] = use_dir + os.path.basename(active_files[active_type])
             self.sample_data[sample][active_type] = sample_dir + os.path.basename(active_files[active_type])
 
             for action in self.params:
@@ -427,7 +426,7 @@ mkdir {temp}
 mv {files} \\
 \t{temp}
 
-find {usedir} -maxdepth 1 -not -name "temp" -exec rm -rf {{}} \;
+find {usedir} -maxdepth 1 -mindepth 1 -not -name "temp" -exec rm -rf {{}} \;
 
 mv {temp}/* \\
 \t{usedir}
